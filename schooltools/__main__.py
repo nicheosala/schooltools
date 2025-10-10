570d2acd96bacdc53d2cc1ce005b0840aa7edfa9
Nicolò Sala <me@nicheosala.info>
2025-10-10 16:14:18 +0200

    Ottobre 2025

--- schooltools/__main__.py
+++ schooltools/__main__.py
@@ -1,4 +1,5 @@
-from csv import DictWriter, reader
+from csv import reader, writer
+from os import replace
 from random import randint, sample
 from typing import Iterable, Iterator
 
@@ -7,7 +8,6 @@ from more_itertools import chunked
 
 
 def scrape(html_file_path: str) -> list[str]:
-
     students: list[str] = []
 
     with open(html_file_path, "r") as f:
@@ -19,11 +19,58 @@ def scrape(html_file_path: str) -> list[str]:
     return students
 
 
+def sanitize(s: str) -> str:
+    return s.lower().replace("'", "").replace(".", "")
+
+
 def write_to_csv(csv_file_path: str, students: list[str]) -> None:
     with open(csv_file_path, "w", newline="") as f:
-        w = DictWriter(f, ["Studente"])
+        fields = [
+            "Nome utente",
+            "Nome",
+            "Cognome",
+            "Nome visualizzato",
+            "Posizione",
+            "Reparto",
+            "Numero ufficio",
+            "Telefono ufficio",
+            "Cellulare",
+            "Fax",
+            "Indirizzo di posta elettronica alternativo",
+            "Indirizzo",
+            "Città",
+            "Stato o provincia",
+            "CAP",
+            "Paese o area geografica",
+        ]
+        w = writer(f, delimiter=",")
+        w.writerow(fields)
         for s in students:
-            w.writerow({"Studente": s})
+            parts = s.split()
+            cognome = parts[0]
+            nomi = parts[1:]
+            nome_utente = (
+                f"{sanitize(''.join(nomi))}.{sanitize(cognome)}@istitutobachelet.edu.it"
+            )
+            w.writerow(
+                [
+                    nome_utente,
+                    " ".join(nomi),
+                    cognome,
+                    s,
+                    "Studente",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            )
 
 
 def read_from_csv(csv_file_path: str) -> list[str]:
@@ -56,3 +103,11 @@ def extract(students: Iterable[str]) -> str:
 def register(students: list[str]) -> None:
     for i, s in enumerate(students, 1):
         print(i, s)
+
+
+if __name__ == "__main__":
+    studenti = scrape("classe.html")
+    for s in studenti:
+        print(s)
+    #for i, g in enumerate(create_random_groups(studenti, 3), 1):
+    #    print(f"| {i} | {', '.join(g)}")
